52121a9b59572ebf9cd37b5f861fdd76e026679e
katayama <tattu.0310@gmail.com>
2022-04-17 15:58:43 +0900

    ⚡️

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,23 @@
 import "src/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className="mx-auto max-w-prose">
+      <Link href="/">
+        <a>
+          <h1 className="my-5 py-5 text-5xl font-bold text-gray-500">
+            katayamaのITブログ
+          </h1>
+        </a>
+      </Link>
+      <hr />
+      <main>
+        <Component {...pageProps} />
+      </main>
+    </div>
+  );
 }
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,36 @@
-import type { NextPage } from "next";
+import { MicroCMSListResponse } from "microcms-js-sdk/dist/cjs/types";
+import type { GetStaticProps, NextPage } from "next";
+import Link from "next/link";
+import { client } from "src/libs/client";
 
-const Home: NextPage = () => {
-  return <div className="text-blue-500">Hello!</div>;
+export type Blog = {
+  title: string;
+  body: string;
+};
+const Home: NextPage<MicroCMSListResponse<Blog>> = (props) => {
+  console.log(props.contents);
+
+  return (
+    <div className="text-blue-500">
+      <p className="my-3 py-3 text-gray-500">{`記事の総数:${props.totalCount}件`}</p>
+      <ul>
+        {props.contents.map((content) => {
+          return (
+            <li key={content.id}>
+              <Link href={`/blog/${content.id}`}>
+                <a>{content.title}</a>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const data = await client.getList({ endpoint: "blog" });
+  return { props: data };
 };
 
 export default Home;
