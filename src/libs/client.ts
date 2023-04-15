adbd91d38e0ae1c9288750e63df7b570625299c1
katayama8000 <tattu.0310@gmail.com>
2023-04-15 14:19:57 +0900

    init

--- src/libs/client.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from "microcms-js-sdk"; //ES6
-
-// Initialize Client SDK.
-export const client = createClient({
-  serviceDomain: "katayama-blog", // YOUR_DOMAIN is the XXXX part of XXXX.microcms.io
-  apiKey: process.env.API_KEY, // YOUR_API_KEY is the YYYY part of XXXX.microcms.io
-});
