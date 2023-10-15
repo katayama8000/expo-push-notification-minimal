5559671e7fb73f38a5b88c025ef9e6a59bf86408
katayama8000 <tattu.0310@gmail.com>
2023-10-15 12:54:46 +0900

    react-hook-form

--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,27 +1,26 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { Button, View, Text, FlatList } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { NavigationProp } from '../../App';
+import { object, string } from 'yup';
+
+const schema = object({
+  destination: string().required(),
+});
 
 export const HomeScreen: FC = () => {
   const { navigate } = useNavigation<NavigationProp>();
+  const [date, setDate] = useState<Date>(new Date());
+  const [open, setOpen] = useState<boolean>(false);
 
   return (
     <View style={{ flex: 1 }}>
       <Button
-        title="新規作成"
+        title="about"
         onPress={() => {
           navigate('About');
         }}
       />
-      {/* <FlatList
-        data={data}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id.toString()}
-        ListEmptyComponent={<Empty />}
-        onRefresh={refreshing}
-        refreshing={isRefresh}
-      /> */}
     </View>
   );
 };
